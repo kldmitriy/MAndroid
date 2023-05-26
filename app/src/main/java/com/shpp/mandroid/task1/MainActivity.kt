c485c79039ab056ac7f39ac8b02019409979fef0
Yevhenii Maslak <greezun@gmail.com>
2023-05-26 21:47:34 +0300

    review student

--- app/src/main/java/com/shpp/mandroid/task1/MainActivity.kt
+++ app/src/main/java/com/shpp/mandroid/task1/MainActivity.kt
@@ -4,23 +4,27 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import com.shpp.mandroid.task1.databinding.ActivityMainBinding
 
+// TODO Binding
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        overridePendingTransition(R.anim.slide_in, R.anim.slide_out)
         displayName()
     }
 
     private fun displayName() {
-        val textviewName = findViewById<TextView>(R.id.text_name)
-        textviewName.text = parseEmail(intent.getStringExtra(getString(R.string.GLOBAL_EMAIL)))
+
+        binding.textName.text = parseEmail(intent.getStringExtra(SignUp.GLOBAL_EMAIL))
     }
 
+    //TODO regex to constants| uppercase
     private fun parseEmail(email: String?): CharSequence? {
-        return Regex("""@[\w.]*""").replace(email.toString(), "").
-            replace(".", " ")
+        return Regex("""@[\w.]*""").replace(email.toString(), "").replace(".", " ")
     }
 }
--- app/src/main/java/com/shpp/mandroid/task1/SignUp.kt
+++ app/src/main/java/com/shpp/mandroid/task1/SignUp.kt
@@ -10,19 +10,27 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.checkbox.MaterialCheckBox
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.shpp.mandroid.task1.databinding.ActivitySignUpBinding
 
 
+//TODO constants
+const val PASSWORD_MIN_LENGTH: Int = 8
+const val STORAGE_LOGIN: String = "LoginDataStorage"
+const val STORAGE_LOGIN_REMEMBER: String = "Remember"
+const val STORAGE_LOGIN_EMAIL: String = "Email"
+//TODO binding
 class SignUp : AppCompatActivity() {
-    private val PASSWORD_MIN_LENGTH: Int = 8
-    private val STORAGE_LOGIN: String = "LoginDataStorage"
-    private val STORAGE_LOGIN_REMEMBER: String = "Remember"
-    private val STORAGE_LOGIN_EMAIL: String = "Email"
 
+    private lateinit var binding: ActivitySignUpBinding
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+
         val lSettings: SharedPreferences = getSharedPreferences(STORAGE_LOGIN, Context.MODE_PRIVATE)
 
         if (lSettings.contains(STORAGE_LOGIN_REMEMBER)) {
@@ -31,12 +39,16 @@ class SignUp : AppCompatActivity() {
             }
         }
 
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+
         overridePendingTransition(R.anim.slide_in, R.anim.slide_out)
 
         val editEmail = findViewById<TextInputLayout>(R.id.edit_email)
         val editEmailText = findViewById<TextInputEditText>(R.id.edit_email_text)
+
+
         editEmailText.addTextChangedListener(object : TextWatcher {
 
             override fun afterTextChanged(s: Editable) {}
@@ -49,23 +61,36 @@ class SignUp : AppCompatActivity() {
             }
         })
 
-        val editPassword = findViewById<TextInputLayout>(R.id.edit_password)
-        val editPasswordText = findViewById<TextInputEditText>(R.id.edit_password_text)
-        editPasswordText.addTextChangedListener(object : TextWatcher {
 
-            override fun afterTextChanged(s: Editable) {}
 
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        binding.editPasswordText.doOnTextChanged { text, start, before, count ->
+            binding.editPassword.error =
+                if (!isPasswordLengthEnough(text.toString()))
+                    getString(R.string.ERROR_SHORT_PASSWORD)
+                else if (isPasswordValid(text.toString())) null
+                else getString(R.string.ERROR_NOT_VALID_PASSWORD)
+        }
 
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                val password = editPasswordText.text.toString()
-                editPassword.error =
-                    if (!isPasswordLengthEnough(password))
-                        getString(R.string.ERROR_SHORT_PASSWORD)
-                    else if (isPasswordValid(password)) null
-                    else getString(R.string.ERROR_NOT_VALID_PASSWORD)
-            }
-        })
+        setListeners()
+    }
+
+    private fun setListeners() {
+       binding.buttonRegister.setOnClickListener {
+           val email = findViewById<TextInputEditText>(R.id.edit_email_text).text.toString()
+           val password = findViewById<TextInputEditText>(R.id.edit_password_text).text.toString()
+
+           if (isEmailValid(email) && isPasswordLengthEnough(password) && isPasswordValid(password)) {
+               if (findViewById<MaterialCheckBox>(R.id.check_remember_me).isChecked) {
+                   val lSettings: SharedPreferences =
+                       getSharedPreferences(STORAGE_LOGIN, Context.MODE_PRIVATE)
+                   val editor: Editor = lSettings.edit()
+                   editor.putBoolean(STORAGE_LOGIN_REMEMBER, true)
+                   editor.putString(STORAGE_LOGIN_EMAIL, email)
+                   editor.apply()
+               }
+               changeActivity(email)
+           }
+       }
     }
 
     private fun isEmailValid(email: String): Boolean {
@@ -80,26 +105,15 @@ class SignUp : AppCompatActivity() {
         return password.count() >= PASSWORD_MIN_LENGTH
     }
 
-    fun onClickRegister(view: View) {
-        val email = findViewById<TextInputEditText>(R.id.edit_email_text).text.toString()
-        val password = findViewById<TextInputEditText>(R.id.edit_password_text).text.toString()
 
-        if (isEmailValid(email) && isPasswordLengthEnough(password) && isPasswordValid(password)) {
-            if (findViewById<MaterialCheckBox>(R.id.check_remember_me).isChecked) {
-                val lSettings: SharedPreferences =
-                    getSharedPreferences(STORAGE_LOGIN, Context.MODE_PRIVATE)
-                val editor: Editor = lSettings.edit()
-                editor.putBoolean(STORAGE_LOGIN_REMEMBER, true)
-                editor.putString(STORAGE_LOGIN_EMAIL, email)
-                editor.apply()
-            }
-            changeActivity(email)
-        }
-    }
 
     private fun changeActivity(email: String) {
         val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra(getString(R.string.GLOBAL_EMAIL), email)
+        intent.putExtra(GLOBAL_EMAIL, email)
         startActivity(intent)
+    }
+
+    companion object{
+        const val GLOBAL_EMAIL  = "email"
     }
 }
